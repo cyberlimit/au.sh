993192a67a8f86afcf19a84485d706b3e8d9d605
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-16 12:41:30 +0700

    Create Au.sh

---
+++ Au.sh
@@ -0,0 +1,336 @@
+#!/system/xbin/Bash
+#JnganRecodeAnjg
+#BikinSendiri
+#Bangsatt
+#JanganLupaToxic
+
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+
+figlet "CyberLimit"| lolcat
+
+cowsay -f eyes **Welcome To Tools** | lolcat
+
+echo " Time : " $TIME | lolcat
+date | lolcat
+
+echo -e "\e[0;36m'¥^=°=°=°°=°=°=°°=°=°°=°=°=°=°=°=°=°>
+echo -e "\e[31;1mAuthor   : CyberLimit"
+echo -e "\e[33;1mkontak me: 089614686780"
+echo -e "\e[0;36mMy Team. : Wisnu | Panjull | Rehan"
+echo -e "\e[36;1mGithub.  : cyberlimit"
+echo -e "\e[0;32mYoutube. : Cyber Limit"
+echo -e "\e[0;36m'¥^=°=°=°=°=°=°=°=°°°=°=°==°=°=°=°=°>
+
+echo -e "\e[33;1m#Note: Hargai Yang Membuat Tools Beg>
+echo ""
+echo -e "\e[0;36mTOOLS INI DIBUAT UNTUK KALIAN\n\tYAN>
+echo ""
+echo "OH IYA SEBELUM GW KASIH SCRIPTNYA LU SUBSCRIBE >
+sleep 5
+echo "GUA ITUNG SAMPE 5 YA"
+sleep 2
+echo "1"
+sleep 2
+echo "2"
+sleep 2
+echo "3"
+sleep 2
+echo "4"
+sleep 2
+echo "5"
+sleep 2
+echo "DUAR MEMEW"
+sleep 2
+echo "OKE THANKS"
+sleep 1
+echo "NIH SCRIPTNYA"
+echo ""
+echo ""
+
+('clear')
+
+echo "£Hack Fb£"
+echo ""
+echo " apt update"
+echo  "apt upgrade"
+echo  "pkg install python2"
+echo  "pkg install git"
+echo  "pip2 install requests"
+echo  "pip2 install mechanize"
+echo  "git clone https://github.com/cyberlimit/darkcy>
+echo  "cd darkcyber"
+echo  "python2 Limit.py"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "^Scan Ip web^"
+echo  "pkg install git python2"
+echo  "pip2 install requests socket"
+echo  "git clone https://github.com/Bl4ckDr460n/ScanI>
+echo  "cd ScanIp"
+echo  "python2 scanip.py"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "^Lacak Ip^"
+echo "pkg install git python2"
+echo "pip2 install requests "
+echo "git clone https://github.com/Bl4ckDr460n/IP-Loc>
+echo "cd IP-Location"
+echo "python2 lacak.py"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "SC DDOS MUSLIM CYBER INDONESIA"
+echo "apt update && apt upgrade"
+echo "apt install python"
+echo "apt install python2"
+echo "apt install git"
+echo "git clone https://github.com/MrTamfanX/MCIDDOS"
+echo "cd MCIDDOS"
+echo "ls "
+echo "python2 TamfanDdos.py"
+echo "python2 TamfanDdos.py [IP Target] [8080] [10000>
+
+echo "Di Cd-nya MCIDDOS itu menggunakan i Besar Bukan>
+"I LOVE YOU PARA MEMBER MUSLIM CYBER INDONESIA"
+
+echo "===============================================>
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "14.SC TROJAN MRTAMFANX CYBER TEAM"
+echo ""
+echo "SC TROJAN MRTAMFANX CYBER TEAM"
+echo "apt update && apt upgrade"
+echo "apt install bash"
+echo "apt install python python2"
+echo "apt install git"
+echo "git clone https://github.com/MrTamfanX/MrDdos"
+echo "cd MrDdos"
+echo "ls"
+echo "chmod +x *"
+echo "ls"
+echo "sh Tamfan-Ddos.sh"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "15.Spam SMS - Call By MrTamfanX 100% Work No Ph>
+
+echo "apt update && apt upgrade"
+echo "apt install bash"
+echo "apt install php"
+echo "apt install git"
+echo "git clone https://github.com/MrTamfanX/MrTamfan>
+echo "cd MrTamfanSpam"
+echo "ls"
+echo "sh MrTamfanSpam.sh"
+echo ""
+echo "==================================≠============>
+echo ""
+sleep 1
+echo "16.Hack IG Hack FB"
+echo "apt update && apt upgrade"
+echo "apt install php git"
+echo "apt install bash"
+echo "git clone https://github.com/MrTamfanX/TamfanFa>
+echo "cd TamfanFacebook"
+echo "chmod +x *"
+echo "chmod -R 775 lib"
+echo "sh install.sh"
+echo "sh tamfan.sh"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "17.TOOLS MRTAMFANX 98% WORK BERISI 300 TOOLS V.>
+"TOOLS MRTAMFANX 98% "
+echo "apt update && apt upgrade"
+echo "apt install bash"
+echo "apt install php"
+echo "apt install python python2"
+echo "apt install git"
+echo "git clone https://github.com/MrTamfanX/TOOLSMRT>
+echo "cd TOOLSMRTAMFAN"
+echo "ls"
+echo "chmod +x *"
+echo "ls"
+echo "sh MrTamfan.sh"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "18.Tembak Kuota"
+echo "apt update && apt upgrade"
+echo "apt install bash"
+echo "apt install python python2"
+echo "apt install php"
+echo "apt install git"
+echo "git clone https://github.com/MrTamfanX/TembakKu>
+echo "cd TembakKuota"
+echo "chmod +x *"
+echo "sh IsiKuota.sh"
+echo "Note : sc ini rusak"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "19.Hack Pulsa Gratis Via Termux"
+
+echo "apt update && apt upgrade"
+echo "apt install php"
+echo "apt install toilet"
+echo "apt install git"
+echo "git clone https://github.com/MrUncle/PulsaGrati>
+echo "cd PulsaGratis"
+echo "ls"
+echo "chmod +x *"
+echo "ls"
+echo "sh Pulsa.sh"
+echo ""
+echo"================================================>
+echo ""
+sleep 1
+echo "20.Hack Bank"
+
+echo "apt update && apt upgrade"
+echo "apt install php"
+echo "apt install git"
+echo "git clone https://github.com/MrUncle/Script-Hac>
+echo "cd Script-HackBank"
+echo "ls"
+echo "chmod +x *"
+echo "ls"
+echo "sh ALLBANK"
+echo "Sc rusak"
+echo ""
+echo "==============================================="
+echo ""
+sleep 1
+echo "21.sadap wa V.6"
+echo"*_=====Sadap WA=====_*"
+echo "pkg install git python2"
+echo "git clone https://github.com/Bl4ckDr460n/HxWhat>
+echo  "cd HxWhatsapp"
+echo  "python2 HxWhatsApp.py"
+echo ""
+echo "==============================================="
+echo ""
+sleep 1
+echo "22.hack fb"
+echo "pkg update && pkg upgrade -y"
+echo "pkg install python2"
+echo "pkg install git"
+echo "git clone https://github.com/rezadkim/MBFz"
+echo "cd MBFz"
+echo "pip2 install -r nganunymous.txt"
+echo "pip2 install mechanize"
+echo "pip2 install progress"
+echo "python2 mbfz.py"
+echo ""
+echo "==============================================="
+echo ""sleep 1
+echo "23.Hacking"
+"FAKE CALL"
+echo "pkg update && pkg upgrade -y"
+echo "pkg install toilet"
+echo "pkg install openssh"
+echo "pkg install php"
+echo "pkg install nodejs"
+echo "pkg install nodejs-lts"
+echo "pkg install python python2"
+echo "pkg install python2-dev"
+echo "pkg install git"
+echo "pkg install curl"
+echo "pkg install figlet"
+echo "pkg install ruby"
+echo "gem install lolcat"
+echo "git clone https://github.com/T14RB373T/FAKECALL"
+echo "cd FAKECALL"
+echo "bash call.sh"
+echo ""
+sleep 1
+echo""
+echo "24. Hack fb"
+
+echo "pkg update && pkg upgrade -y"
+echo "pkg install python2"
+echo "pkg install git"
+echo "git clone https://github.com/rezadkim/MBFz"
+echo "cd MBFz"
+echo "pip2 install -r nganunymous.txt"
+echo "pi5p2 install mechanize"
+echo "pip2 install progress"
+echo "python2 mbfz.py"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "25.Cara hack ig"
+
+echo "pkg update"
+echo "pkg upgrade"
+echo "pkg install git"
+echo "pkg install python python2"
+echo "git clone https://github.com/linuxkukeren/insta>
+echo "cd instagram"
+echo "pip install -r requirements.txt"
+echo "pip2 install -r requirements.txt"
+echo "python2 linuxkerenhackig.py (nama target) pass.>
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "26.Hack Instagram"
+echo "Ok gan nih kode atau kata perintahnya :"
+echo "pkg update"
+echo "pkg upgrade"
+echo "pkg install git"
+echo "pkg install nano"
+echo "pkg install cowsay"
+echo "pkg install figlet"
+echo "pkg install python2"
+echo "gem install lolcat"
+echo "git clone https://github.com/DarkCurut08/DarkCu>
+echo "cd DarkCurut08"
+echo "chmod +x Moreno777.sh"
+echo "sh Moreno77.sh"
+echo "Jangan Lupa Install Bahan Dlu Di No 8"
+echo ""
+echo "===============================================>
+echo ""
+sleep 1
+echo "27.Termux commands (root)"
+echo "apt update"
+echo "apt upgarde"
+echo "apt install git"
+echo "git clone https://github.com/thelinuxchoice/ins>
+echo "ls "
+echo "cd instainsane"
+echo "ls"
+echo "chmod +x *"
+echo "ls"
+echo "./install.sh"
+echo "./instainsane.sh"
+echo ""
+echo ""
+
+echo "NAH SUBSCRIBE CHANELL GW ASUU"
+echo ""
+echo "NIH LINK NYA https://m.youtube.com/channel/UCjj>
+echo ""
+echo "AUTOR :| WISNU | PANJULL|"
+echo "Youtube : Cyber Limit"
