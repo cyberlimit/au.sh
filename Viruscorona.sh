bb8c779ccec9e6612b7cfe683891c282051ab81b
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-26 18:58:31 +0700

    Create Viruscorona.sh

---
+++ Viruscorona.sh
@@ -0,0 +1,165 @@
+#!/bin/Bash
+#no recode anjg
+#Hacked bye cyber
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+read -p "TADI NAMA LU SAPA : " pil
+
+clear
+
+read -p "APA ANDA YAKIN MENGGUNAKAN VIRUS CORONA : " pil
+
+clear
+echo ""
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№"
+echo "№.  Autor : CyberLimit.             №"
+echo "№. My Team: Wisnu | panjul.         №"
+echo "№. Youtube: Cyber Limit.            №"
+echo "№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№"
+echo ""
+echo "#Note: HARGAI YANG BUAT!!\n\tVIRUS INI BERKERJA 1X24 JAM"
+echo ""
+read -p "MASUKAN NOMER TARGET : " pil
+echo ""
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo -e "$red VIRUS CORONA BERHASIL DI KIRIM "
+sleep 1
+echo ".  VIRUS SELESAI DI KIRIM"
+
+
